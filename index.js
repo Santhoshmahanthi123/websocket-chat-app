6773070f0ae38cd03c938aeaae07858566b2043c
santhosh <singumahanthisanthosh@gmail.com>
2018-10-05 17:27:06 +0530

    making web socket connection and disconnection

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const socket = require('socket.io');
 //set up express app
 const app = express();
 var server = app.listen(3030,()=>
 console.log("listening to server on port:3030"));
 //static files
 app.use(express.static('public'));
+ //Setting up the socket to the back end express server
+const io = socket(server);
+//When every client connects to socket this function gets executed
+io.on('connection',(socket)=>{
+    //socket.id gives the unique id every time new user connects to the socket
+    console.log("A user connected!",socket.id);
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+        console.log('A user disconnected!',socket.id);
+      });
+    
+});
