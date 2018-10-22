ee1427843636805b58aa9b28465d7d5be740043e
santhosh <singumahanthisanthosh@gmail.com>
2018-10-22 20:55:55 +0530

    heading and styling updated

---
+++ public/chat.js
@@ -0,0 +1,31 @@
+//Make Connection
+const socket = io.connect("http://localhost:3030");
+//Query DOM
+const message = document.getElementById('message');
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
+//Adding event listener to the button
+
+btn.addEventListener('click',()=>{
+   //this tells about the name of the message and data of the message is to be sent
+    socket.emit('chat',{
+        message:message.value,
+        handle:handle.value
+    });
+});
+
+message.addEventListener('keypress',()=>{
+    socket.emit('typing',handle.value);
+});
+
+//Listen for the events on front end
+
+socket.on('chat',(data)=>{
+    feedback.innerHTML ="";
+output.innerHTML +='<p><strong>'+ data.handle +':</strong>' + data.message +'</p>'
+});
+socket.on('typing',(data)=>{
+    feedback.innerHTML = '<p><em>' + data +' is typing a message...</em></p>';
+})
